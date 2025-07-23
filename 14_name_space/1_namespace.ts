2ddf0c861caefa50bfa42f3d8d531fa80cd7dbed
sujin.park <sujin.park@spc.co.kr>
2025-07-23 16:50:11 +0900

    namespace

---
+++ 14_name_space/1_namespace.ts
@@ -0,0 +1,47 @@
+/**
+ * namespace
+ */
+
+namespace Home {
+    class Idol {
+        name: string;
+        age: number;
+
+        constructor(name: string, age: number) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+    export const yuJin = new Idol("안유진", 20);
+}
+
+namespace Post {
+    class User {
+        email: string;
+        name: string;
+
+        constructor(email: string, name: string) {
+            this.email = email;
+            this.name = name;
+        }
+    }    
+
+    const admin = new User("admin@gmail.com", Home.yuJin.name);
+    console.log(admin);
+}
+
+namespace Comment {
+    const name = 'comment';
+
+    namespace Detail {
+        const page = 'detail';
+        console.log(name);
+        console.log(page);
+    }
+
+    console.log('=-------------=');
+    console.log(name);
+    // console.log(Detail.page);
+
+}
